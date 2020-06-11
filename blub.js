c2df567e1906653bbe9eae6b2ef5d8383f66ba3e
zain_aj1 <zain_aj1@hotmail.com>
2020-06-11 11:21:35 -0400

    test

--- blub.js
+++ blub.js
@@ -16,34 +16,71 @@ function connect(){
     .then(device => { 
         // Get name of device
         console.log(device.name);
-    
+
         // Connect to remote GATT Server
         return device.gatt.connect();
     })
     .then(server => {
-        // Access light turning on/off service
-        console.log("Getting primary service " + serv);
-        return server.getPrimaryService(serv);
-    })
-    .then(service => {
-        // Get characteristic to communicate with
-        console.log("Getting characteristic " + charc);
-        return service.getCharacteristic(charc);
-    })
-    .catch(error => {console.log(error); });
+        // Note that we could also get all services that match a specific UUID by
+        // passing it to getPrimaryServices().
+        log('Getting Services...');
+        return server.getPrimaryServices();
+        })
+        .then(services => {
+        log('Getting Characteristics...');
+        let queue = Promise.resolve();
+        services.forEach(service => {
+            queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+            log('> Service: ' + service.uuid);
+            characteristics.forEach(characteristic => {
+                log('>> Characteristic: ' + characteristic.uuid + ' ' +
+                    getSupportedProperties(characteristic));
+            });
+            }));
+        });
+        return queue;
+        })
+        .catch(error => {
+        log('Argh! ' + error);
+        });
+    }
+    
+/* Utils */
+    
+function getSupportedProperties(characteristic) {
+    let supportedProperties = [];
+    for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+        supportedProperties.push(p.toUpperCase());
+    }
+    }
+    return '[' + supportedProperties.join(', ') + ']';
 }
 
-// User input for serivce
-function setService(){
-    serv = parseInt(document.getElementById("service").value, 16);
-    console.log("service set to: "+serv.toString(16));
-}
+//     .then(server => {
+//         // Access light turning on/off service
+//         console.log("Getting primary service " + serv);
+//         return server.getPrimaryService(serv);
+//     })
+//     .then(service => {
+//         // Get characteristic to communicate with
+//         console.log("Getting characteristic " + charc);
+//         return service.getCharacteristic(charc);
+//     })
+//     .catch(error => {console.log(error); });
+// }
 
-// User input for characteristic
-function setCharacteristic(){
-    charc = parseInt(document.getElementById("Characteristic").value, 16);
-    console.log("characteristic set to: "+charc.toString(16));
-}
+// // User input for serivce
+// function setService(){
+//     serv = Number(document.getElementById("service").value);
+//     console.log("service set to: "+serv.toString(16));
+// }
+
+// // User input for characteristic
+// function setCharacteristic(){
+//     charc = Number(document.getElementById("Characteristic").value);
+//     console.log("characteristic set to: "+charc.toString(16));
+// }
 
 
 // Light functions
