ecb075a878de6928e57e957c04d3b2471225c0b9
jaffarys <syed.jaffary@mail.utoronto.ca>
2020-06-10 20:16:21 -0400

    Create connect function
    Connect button should now acc connet something

--- blub.js
+++ blub.js
@@ -1,16 +1,18 @@
 // this tries to find all devices
 // TODO: Add a filter just for the bulbs
-navigator.bluetooth.requestDevice({
-    acceptAllDevices: true
-})
-.then(device => { 
-    // Get name of device
-    console.log(device.name);
-
-    // Connect to remote GATT Server
-    return device.gatt.connet();
-})
-.then(server => {
-    /* ... */
-})
-.catch(error => {console.log(error); });
+function connect(){
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true
+    })
+    .then(device => { 
+        // Get name of device
+        console.log(device.name);
+    
+        // Connect to remote GATT Server
+        return device.gatt.connet();
+    })
+    .then(server => {
+        /* ... */
+    })
+    .catch(error => {console.log(error); });
+}
