e36040a84776f418f2342954be3b602165dca029
zainaj1 <zain_aj1@hotmail.com>
2020-06-12 16:40:06 -0400

    a

--- audio.js
+++ audio.js
@@ -7,58 +7,63 @@ window.onload = function() {
 
     file.onchange = function() {
   
-      const files = this.files; // FileList containing File objects selected by the user (DOM File API)
-      console.log('FILES[0]: ', files[0])
-      audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
-  
-      const name = files[0];
+        const files = this.files; // FileList containing File objects selected by the user (DOM File API)
+        console.log('FILES[0]: ', files[0])
+        audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
 
-      // for legacy browsers
-      const AudioContext = window.AudioContext || window.webkitAudioContext;
-      const context = new AudioContext();
+        const name = files[0];
 
-      let src = context.createMediaElementSource(audio); // Give the audio context an audio source,
-      // to which can then be played and manipulated
-      const analyser = context.createAnalyser(); // Create an analyser for the audio context
-  
-      src.connect(analyser); // Connects the audio context source to the analyser
-      analyser.connect(context.destination); // End destination of an audio graph in a given context
-      // Sends sound to the speakers or headphones
-  
-  
-      /////////////// ANALYSER FFTSIZE ////////////////////////
-      analyser.fftSize = 32;
-      // analyser.fftSize = 64;
-      // analyser.fftSize = 128;
-      // analyser.fftSize = 256;
-      // analyser.fftSize = 512;
+        // for legacy browsers
+
+        if('webkitAudioContext' in window) {
+            var context = new webkitAudioContext();
+        }else{
+            const AudioContext = window.AudioContext || window.webkitAudioContext;
+            var context = new AudioContext();
+        }  
+
+        let src = context.createMediaElementSource(audio); // Give the audio context an audio source,
+        // to which can then be played and manipulated
+        const analyser = context.createAnalyser(); // Create an analyser for the audio context
+
+        src.connect(analyser); // Connects the audio context source to the analyser
+        analyser.connect(context.destination); // End destination of an audio graph in a given context
+        // Sends sound to the speakers or headphones
+
+
+        /////////////// ANALYSER FFTSIZE ////////////////////////
+        analyser.fftSize = 32;
+        // analyser.fftSize = 64;
+        // analyser.fftSize = 128;
+        // analyser.fftSize = 256;
+        // analyser.fftSize = 512;
     //   analyser.fftSize = 1024;
-      // analyser.fftSize = 2048;
-      // analyser.fftSize = 4096;
-      // analyser.fftSize = 8192;
+        // analyser.fftSize = 2048;
+        // analyser.fftSize = 4096;
+        // analyser.fftSize = 8192;
     //   analyser.fftSize = 16384;
     //   analyser.fftSize = 32768;
-  
-      // (FFT) is an algorithm that samples a signal over a period of time
-      // and divides it into its frequency components (single sinusoidal oscillations).
-      // It separates the mixed signals and shows what frequency is a violent vibration.
-  
-      // (FFTSize) represents the window size in samples that is used when performing a FFT
-  
-      // Lower the size, the less bars (but wider in size)
-      ///////////////////////////////////////////////////////////
 
-  
-      const bufferLength = analyser.frequencyBinCount; // (read-only property)
-      // Unsigned integer, half of fftSize (so in this case, bufferLength = 8192)
-      // Equates to number of data values you have to play with for the visualization
-  
-      // The FFT size defines the number of bins used for dividing the window into equal strips, or bins.
-      // Hence, a bin is a spectrum sample, and defines the frequency resolution of the window.
-  
-      const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
-      // At this point dataArray is an array with length of bufferLength but no values
-      console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
+        // (FFT) is an algorithm that samples a signal over a period of time
+        // and divides it into its frequency components (single sinusoidal oscillations).
+        // It separates the mixed signals and shows what frequency is a violent vibration.
+
+        // (FFTSize) represents the window size in samples that is used when performing a FFT
+
+        // Lower the size, the less bars (but wider in size)
+        ///////////////////////////////////////////////////////////
+
+
+        const bufferLength = analyser.frequencyBinCount; // (read-only property)
+        // Unsigned integer, half of fftSize (so in this case, bufferLength = 8192)
+        // Equates to number of data values you have to play with for the visualization
+
+        // The FFT size defines the number of bins used for dividing the window into equal strips, or bins.
+        // Hence, a bin is a spectrum sample, and defines the frequency resolution of the window.
+
+        const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
+        // At this point dataArray is an array with length of bufferLength but no values
+        console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
 
         function renderFrame(){
             analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
