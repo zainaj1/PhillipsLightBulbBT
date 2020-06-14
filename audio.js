89e41f2a4211520b924222acd3c86bfc1e8be0ff
zainaj1 <zain_aj1@hotmail.com>
2020-06-14 11:48:30 -0400

    This is just a test

--- audio.js
+++ audio.js
@@ -1,89 +1,103 @@
+const file = document.getElementById("file-input");
+const audio = document.querySelector('audio');
 
-window.addEventListener('touchstart',  function() {
-    console.log("start");
-    const file = document.getElementById("file-input");
-    const audio = document.querySelector('audio');
+// for legacy browsers
+try
+{
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var mycontext = new AudioContext();
+}
+catch(e)
+{
+    alert("Your browser doesn't support Web Audio API");
+}
 
-    // for legacy browsers
-    try
-    {
-        window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        var context = new AudioContext();
-    }
-    catch(e)
-    {
-        alert("Your browser doesn't support Web Audio API");
-    }
+file.onchange = function() {
+    webAudioTouchUnlock(mycontext);
+    const files = this.files; // FileList containing File objects selected by the user (DOM File API)
+    console.log('FILES[0]: ', files[0])
+    audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
     
-    // Try to unlock the context
-    // create empty buffer
-    context.resume().then(() => {
-        console.log('Playback resumed successfully');
-    });
+    const name = files[0];
 
-    var buffer = context.createBuffer(1, 1, 22050);
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start(0);
+    let src = mycontext.createMediaElementSource(audio); // Give the audio context an audio source,
+    // to which can then be played and manipulated
+    const analyser = mycontext.createAnalyser(); // Create an analyser for the audio context
 
-    file.onchange = function() {
-        const files = this.files; // FileList containing File objects selected by the user (DOM File API)
-        console.log('FILES[0]: ', files[0])
-        audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
-        
-        const name = files[0];
-
-        let src = context.createMediaElementSource(audio); // Give the audio context an audio source,
-        // to which can then be played and manipulated
-        const analyser = context.createAnalyser(); // Create an analyser for the audio context
-
-        src.connect(analyser); // Connects the audio context source to the analyser
-        analyser.connect(context.destination); // End destination of an audio graph in a given context
-        // Sends sound to the speakers or headphones
+    src.connect(analyser); // Connects the audio context source to the analyser
+    analyser.connect(mycontext.destination); // End destination of an audio graph in a given context
+    // Sends sound to the speakers or headphones
 
 
-        /////////////// ANALYSER FFTSIZE ////////////////////////
-        analyser.fftSize = 32;
-        // analyser.fftSize = 64;
-        // analyser.fftSize = 128;
-        // analyser.fftSize = 256;
-        // analyser.fftSize = 512;
-    //   analyser.fftSize = 1024;
-        // analyser.fftSize = 2048;
-        // analyser.fftSize = 4096;
-        // analyser.fftSize = 8192;
-    //   analyser.fftSize = 16384;
-    //   analyser.fftSize = 32768;
+    /////////////// ANALYSER FFTSIZE ////////////////////////
+    analyser.fftSize = 32;
+    // analyser.fftSize = 64;
+    // analyser.fftSize = 128;
+    // analyser.fftSize = 256;
+    // analyser.fftSize = 512;
+//   analyser.fftSize = 1024;
+    // analyser.fftSize = 2048;
+    // analyser.fftSize = 4096;
+    // analyser.fftSize = 8192;
+//   analyser.fftSize = 16384;
+//   analyser.fftSize = 32768;
 
-        // (FFT) is an algorithm that samples a signal over a period of time
-        // and divides it into its frequency components (single sinusoidal oscillations).
-        // It separates the mixed signals and shows what frequency is a violent vibration.
+    // (FFT) is an algorithm that samples a signal over a period of time
+    // and divides it into its frequency components (single sinusoidal oscillations).
+    // It separates the mixed signals and shows what frequency is a violent vibration.
 
-        // (FFTSize) represents the window size in samples that is used when performing a FFT
+    // (FFTSize) represents the window size in samples that is used when performing a FFT
 
-        // Lower the size, the less bars (but wider in size)
-        ///////////////////////////////////////////////////////////
+    // Lower the size, the less bars (but wider in size)
+    ///////////////////////////////////////////////////////////
 
 
-        const bufferLength = analyser.frequencyBinCount; // (read-only property)
-        // Unsigned integer, half of fftSize (so in this case, bufferLength = 8192)
-        // Equates to number of data values you have to play with for the visualization
+    const bufferLength = analyser.frequencyBinCount; // (read-only property)
+    // Unsigned integer, half of fftSize (so in this case, bufferLength = 8192)
+    // Equates to number of data values you have to play with for the visualization
 
-        // The FFT size defines the number of bins used for dividing the window into equal strips, or bins.
-        // Hence, a bin is a spectrum sample, and defines the frequency resolution of the window.
+    // The FFT size defines the number of bins used for dividing the window into equal strips, or bins.
+    // Hence, a bin is a spectrum sample, and defines the frequency resolution of the window.
 
-        const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
-        // At this point dataArray is an array with length of bufferLength but no values
-        console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
+    const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
+    // At this point dataArray is an array with length of bufferLength but no values
+    console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
 
-        function renderFrame(){
-            analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
-            console.log(dataArray[dataArray.length/2]);
+    function renderFrame(){
+        analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
+        console.log(dataArray[dataArray.length/2]);
+    }
+    audio.play();
+    setInterval(renderFrame, 60);
+};
+
+function webAudioTouchUnlock (context)
+{
+    return new Promise(function (resolve, reject)
+    {
+        if (context.state === 'suspended' && 'ontouchstart' in window)
+        {
+            var unlock = function()
+            {
+                context.resume().then(function()
+                {
+                    document.body.removeEventListener('touchstart', unlock);
+                    document.body.removeEventListener('touchend', unlock);
+
+                    resolve(true);
+                }, 
+                function (reason)
+                {
+                    reject(reason);
+                });
+            };
+
+            document.body.addEventListener('touchstart', unlock, false);
+            document.body.addEventListener('touchend', unlock, false);
         }
-       
-        // src.noteOn(0);
-        audio.play();
-        setInterval(renderFrame, 60);
-    };
-  }, false);
+        else
+        {
+            resolve(false);
+        }
+    });
+}
