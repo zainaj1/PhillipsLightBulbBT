a3457367629015697a1a040539bf4a5be6cc01e2
zainaj1 <zain_aj1@hotmail.com>
2020-06-14 19:00:43 -0400

    Maybe might work maybe idk

--- audio.js
+++ audio.js
@@ -1,54 +1,56 @@
-// const file = document.getElementById("file-input");
-// const audio = document.querySelector('audio');
+// window.addEventListener('touchstart', function() {
 
-// // for legacy browsers
-// try
-// {
 //     window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+//     if(mycontext.state === "suspended"){
+//         // Initialize audio context
 //     var mycontext = new AudioContext();
-// }
-// catch(e)
-// {
-//     alert("Your browser doesn't support Web Audio API");
-// }
-
-// const files = this.files; // FileList containing File objects selected by the user (DOM File API)
-// console.log('FILES[0]: ', files[0])
-// audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
-
-// const name = files[0];
-
-// let src = mycontext.createMediaElementSource(audio); // Give the audio context an audio source,
-// // to which can then be played and manipulated
-// const analyser = mycontext.createAnalyser(); // Create an analyser for the audio context
-
-// src.connect(analyser); // Connects the audio context source to the analyser
-// analyser.connect(mycontext.destination); // End destination of an audio graph in a given context
-// // Sends sound to the speakers or headphones
-
-
-// /////////////// ANALYSER FFTSIZE ////////////////////////
-// analyser.fftSize = 32;
-
-
-// const bufferLength = analyser.frequencyBinCount;
-// const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
-
-// // At this point dataArray is an array with length of bufferLength but no values
-// console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
-
-// function renderFrame(){
-//     analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
-//     console.log(dataArray[dataArray.length/2]);
-// }
-
-// if(!mycontext.state === 'suspended'){
+//     // mycontext.resume
+    
+//     // Get the element from the DOM
+//     var audioElement = document.getElementById('audio');
+    
+//     // Create a node based on the element
+//     var src = mycontext.createMediaElementSource(audioElement);
+//     // source.connect(context.destination);
+//     }
+    
 //     audio.play();
-//     setInterval(renderFrame, 60);
-// }else
-// {
-//     console.log("State is suspended");
-// }
+//     mycontext.resume;
+    
+//     console.log(mycontext.state);
+
+//     // to which can then be played and manipulated
+//     const analyser = mycontext.createAnalyser(); // Create an analyser for the audio context
+
+//     src.connect(analyser); // Connects the audio context source to the analyser
+//     analyser.connect(mycontext.destination); // End destination of an audio graph in a given context
+//     // Sends sound to the speakers or headphones
+
+
+//     /////////////// ANALYSER FFTSIZE ////////////////////////
+//     analyser.fftSize = 32;
+
+//     const bufferLength = analyser.frequencyBinCount;
+//     const dataArray = new Uint8Array(bufferLength); // Converts to 8-bit unsigned integer array
+
+//     // At this point dataArray is an array with length of bufferLength but no values
+//     console.log('DATA-ARRAY: ', dataArray) // Check out this array of frequency values!
+
+//     function renderFrame(){
+//         analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
+//         console.log(dataArray[dataArray.length/2]);
+//     }
+
+//     if(mycontext.state === 'running'){
+//         audio.play();
+//         setInterval(renderFrame, 60);
+//     }else
+//     {
+//         console.log("State is suspended");
+//     }
+
+// }, false);
 
 
 let context;
@@ -59,25 +61,34 @@ const audio = document.querySelector('audio');
 
 
 window.addEventListener('touchstart', function() {
-    
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    context = new AudioContext();
-    getData();
-
-    // play the file: This is what unlocks the context
-    context.resume;
+    if (source == null){
+        console.log("Made new context");
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        context = new AudioContext();
+    }
     console.log(context.state);
+    
+    if(context.state === "suspended" && source == null){
+        getData();
+        init();
+        // play the file: This is what unlocks the context 
+    }
+    else if(source != null && context.state === "suspended"){
+        context.resume;
+        source.start(0);
+        audio.play();
+    }
+    
 
-    if( context.state === "running"){
-        play();
-
+    if( context.state === "running" && source != null){
+        console.log("HEre");
         // Initalize nodes
-        var sourceJs = context.createScriptProcessor(2048, 1, 1);
-
-        // Initalize analysier
+         // Initalize analysier
         analyser = context.createAnalyser(); 
         analyser.smoothingTimeConstant = 0.6;
-        analyser.fftSize = 512;
+        analyser.fftSize = 512; 
+
+        var sourceJs = context.createScriptProcessor(2048, 1, 1);
 
         // Connect nodes
         source.connect(analyser);
@@ -88,11 +99,14 @@ window.addEventListener('touchstart', function() {
         sourceJs.connect(context.destination);
         // End destination of an audio graph in a given context
 
-        sourceJs.onaudioprocess = function(e){
-
-            var array = new Uint8Array(analyser.frequencyBinCount);
+        // source.start();
+        // sourceJs.start(0);
+        // analyser.start(0);
+        // audio.play();
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        sourceJs.onaudioprocess = function(audioProcessingEvent){
             analyser.getByteFrequencyData(array);
-            console.log('DATA-ARRAY: ', array) // Check out this array of frequency values!
+            // console.log('DATA-ARRAY: ', array) // Check out this array of frequency values!
             renderFrames(array);
         };
     }
@@ -103,16 +117,18 @@ window.addEventListener('touchstart', function() {
 
 
 function renderFrames(dataArray){
-    console.log(dataArray);
+    console.log(dataArray[dataArray.length/2]);
 } 
 
-function play(){
+function init(){
+    // Initalize source
     source = context.createBufferSource();
     source.buffer = soundBuffer;
     source.connect(context.destination);
 
-    source.start(0);
+    
 }
+
 /**
  * getData
  * Does an XML http request to fetch the required audio file
@@ -133,12 +149,14 @@ function getData(){
         var data = request.response;
 
         context.decodeAudioData(data, function(buffer){
+            console.log(data);
             soubdBuffer = buffer;
         }, 
         function(e){
             console.log("There has been an error: "+ e);
         });
-    };
+    }
+    // request.send();
 }
 
 function getDataFromAudio(){
@@ -157,34 +175,3 @@ function renderFrame(dataArray){
         analyser.getByteFrequencyData(dataArray); // Gets the freqyency data into the data rray
         console.log(dataArray[dataArray.length/2]);
     }
-
-function webAudioTouchUnlock (context)
-{
-    return new Promise(function (resolve, reject)
-    {
-        if (context.state === 'suspended' && 'ontouchstart' in window)
-        {
-            var unlock = function()
-            {
-                context.resume().then(function()
-                {
-                    document.body.removeEventListener('touchstart', unlock);
-                    document.body.removeEventListener('touchend', unlock);
-
-                    resolve(true);
-                }, 
-                function (reason)
-                {
-                    reject(reason);
-                });
-            };
-
-            document.body.addEventListener('touchstart', unlock, false);
-            document.body.addEventListener('touchend', unlock, false);
-        }
-        else
-        {
-            resolve(false);
-        }
-    });
-}
